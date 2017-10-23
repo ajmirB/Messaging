a6c1f832b26962ec2a7544eb5faa1d127d05a961
Ajmir Busgeeth <ajmirulla.busgeeth@shopmium.com>
2017-10-23 15:37:15 +0200

    Channel list : add toolbar

--- app/src/main/java/com/xception/messaging/features/channels/ChannelsActivity.kt
+++ app/src/main/java/com/xception/messaging/features/channels/ChannelsActivity.kt
@@ -3,21 +3,29 @@ package com.xception.messaging.features.channels
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.Toolbar
 import com.xception.messaging.R
 import com.xception.messaging.features.channels.fragments.ChannelListFragment
 import com.xception.messaging.features.channels.fragments.ConversationFragment
+import com.xception.messaging.features.commons.BaseActivity
 
-class ChannelsActivity: AppCompatActivity() {
+class ChannelsActivity: BaseActivity() {
+
+    private lateinit var mTopToolbar: Toolbar
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_fragment)
+        setContentView(R.layout.activity_channels)
+
+        mTopToolbar = findViewById(R.id.toolbar)
+        setToolbarProperties(mTopToolbar)
+
         showChannelList()
     }
 
     fun showChannelList(){
         val channelListFragment = ChannelListFragment.newInstance()
+        title = getString(R.string.channel_list_title)
         supportFragmentManager.beginTransaction()
                 .replace(R.id.activity_fragment_main_container, channelListFragment)
                 .commit()
--- app/src/main/java/com/xception/messaging/features/commons/BaseActivity.kt
+++ app/src/main/java/com/xception/messaging/features/commons/BaseActivity.kt
@@ -1,6 +1,35 @@
 package com.xception.messaging.features.commons
 
+import android.support.v4.content.ContextCompat
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.Toolbar
+import android.widget.TextView
+import com.xception.messaging.R
 
 
-open class BaseActivity: AppCompatActivity()
+open class BaseActivity: AppCompatActivity() {
+
+    protected var mToolbarTitleTextView: TextView? = null
+
+    override fun setTitle(title: CharSequence?) {
+        super.setTitle(title)
+        mToolbarTitleTextView?.text = title?.toString()?.toUpperCase()
+    }
+
+    /**
+     * When the activity have a custom toolbar in activity xml, it configure to the default properties
+     * @param toolbar the toolbar in the activity xml
+     */
+    protected fun setToolbarProperties(toolbar: Toolbar) {
+        setSupportActionBar(toolbar)
+
+        mToolbarTitleTextView = findViewById(R.id.toolbar_title_text_view)
+
+        supportActionBar!!.setDisplayShowTitleEnabled(false)
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+
+        val toolbarItemColor = ContextCompat.getColor(this, R.color.toolbar_item_color)
+        toolbar.navigationIcon?.setColorFilter(toolbarItemColor, android.graphics.PorterDuff.Mode.MULTIPLY)
+        toolbar.setNavigationOnClickListener({ onBackPressed() })
+    }
+}
